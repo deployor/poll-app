7e3311ef3877e0d531d20d7f13715f88d3425764
Tom Daamen <tom.daamen@schule1.de>
2024-12-01 22:35:03 +0100

    FIX ACCORDING TO CLUADE

--- next.config.js
+++ next.config.js
@@ -2,6 +2,13 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ["utf.io"],
+    domains: ['utfs.io'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'utfs.io',
+        pathname: '/**',
+      },
+    ],
   },
 };
